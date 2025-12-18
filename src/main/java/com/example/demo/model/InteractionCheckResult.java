fd4aa7dfd6ffad01ab942ddcb8a20780d7b4eb5a
bala-amypo <bala@amypo.in>
2025-12-18 09:10:55 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionCheckResult.java
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -14,11 +14,15 @@ public class User{
     @NotBlank(message = "Name is required")
     @Size(min = 2,max = 100,message = "Name must be between 2 and 100 characters")
     @Column(nullable =  false)
+    private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     @Column(nullable =  false, unique= true)
     private String email;
 
-    @Not
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Password msut be at least 6 characters")
+    @Column(nullable = false)
+    private String password;
 }
