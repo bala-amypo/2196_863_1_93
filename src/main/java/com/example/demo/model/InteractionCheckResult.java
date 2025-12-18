9c940f4334e2f79f1eaf9ea108224e62cd4c64b3
bala-amypo <bala@amypo.in>
2025-12-18 09:00:55 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionCheckResult.java
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -1,1 +1,13 @@
 package com.example.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "users")
+public class User{
+
+    @Id
+    @GenerateValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
