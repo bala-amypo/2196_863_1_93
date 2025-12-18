91c05711092e9559e5d55c2a4bab4d5a3de2607c
bala-amypo <bala@amypo.in>
2025-12-18 09:20:55 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionCheckResult.java
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -42,5 +42,29 @@ public class User{
     public Long getId(){
         return id;
     }
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 
 }
