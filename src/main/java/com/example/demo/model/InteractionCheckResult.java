14c33b0dbd8e11ed903c9fd20216bb562427f118
bala-amypo <bala@amypo.in>
2025-12-18 09:05:55 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionCheckResult.java
+++ src/main/java/com/example/demo/model/InteractionCheckResult.java
@@ -10,4 +10,15 @@ public class User{
     @Id
     @GenerateValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2,max = 100,message = "Name must be between 2 and 100 characters")
+    @Column(nullable =  false)
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(nullable =  false, unique= true)
+    private String email;
+
+    @Not
 }
