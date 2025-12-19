b5a9d6cc3106cfad3056e6f4e87db94589eebf7d
bala-amypo <bala@amypo.in>
2025-12-19 06:17:46 +0000

    User commit

--- src/main/java/com/example/demo/model/Medication.java
+++ src/main/java/com/example/demo/model/Medication.java
@@ -5,6 +5,16 @@ import java.util.*;
 @Entity
 @Table(name = "medications")
 public class Medication{
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable =  false)
+    private String name;
+
+    @ManyToMany
+    @JoinTable{
+        name = "medication_ingr"
+    }
 }
 
