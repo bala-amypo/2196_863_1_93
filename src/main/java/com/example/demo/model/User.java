8139a25029b7b63d2853bbe8d71e1b33ccb84857
bala-amypo <bala@amypo.in>
2025-12-19 04:47:34 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,7 +1,6 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 
 @Entity
 @Table(name = "users")
@@ -11,23 +10,17 @@ public class User{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name is required")
-    @Size(min = 2,max = 100,message = "Name must be between 2 and 100 characters")
     @Column(nullable =  false,length = 100)
     private String name;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
     @Column(nullable =  false, unique= true,length = 150)
     private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 6,message = "Password must be at least 6 characters")
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
-    private String role = "USER";
+    private String role;
 
     public User(){
         
