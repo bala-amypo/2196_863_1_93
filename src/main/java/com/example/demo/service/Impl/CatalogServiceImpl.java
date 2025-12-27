0e182257ac879dea66f000a1ba9111cc0e0d8ab0
bala-amypo <bala@amypo.in>
2025-12-27 01:36:56 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/CatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CatalogServiceImpl.java
@@ -5,14 +5,22 @@ import com.example.demo.model.Medication;
 import com.example.demo.repository.ActiveIngredientRepository;
 import com.example.demo.repository.MedicationRepository;
 import com.example.demo.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class CatalogServiceImpl implements CatalogService {
     
-    private final ActiveIngredientRepository ingredientRepository;
-    private final MedicationRepository medicationRepository;
+    @Autowired
+    private ActiveIngredientRepository ingredientRepository;
+    
+    @Autowired
+    private MedicationRepository medicationRepository;
+    
+    // ADD THIS NO-ARG CONSTRUCTOR
+    public CatalogServiceImpl() {
+    }
     
     public CatalogServiceImpl(ActiveIngredientRepository ingredientRepository, 
                              MedicationRepository medicationRepository) {
